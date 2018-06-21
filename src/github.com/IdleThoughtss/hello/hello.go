68c5478fdee346941e6729e82197b404d886a11b
Zhurenkai <zhurkai@163.com>
2018-06-21 15:12:19 +0800

    删除测试

--- src/github.com/IdleThoughtss/hello/hello.go
+++ src/github.com/IdleThoughtss/hello/hello.go
@@ -2,26 +2,16 @@ package main
 
 import (
 	"github.com/IdleThoughtss/wechat"
-	"os"
-	"encoding/json"
 	"fmt"
 )
 
 func main(){
 	wx := wechat.NewInstance()
 	wx.Start()
-	fh,_ := os.Create(`tmp.aaa`)
-	c,_ := json.Marshal(wx.ContactList)
-	n,_ := fh.Write(c)
-	fmt.Print(n)
-	//wx.HandleMessage(handler)
-
+	wx.HandleMessage(handler)
 
 }
 
 func handler(wx *wechat.Server,message wechat.Msg)  {
-		if message.From.RemarkName == `静静宝贝` {
-			message.Reply(`你别回来了！`)
-		}
+	fmt.Println(message.From.NickName + `:` + message.Content)	
 }
-
